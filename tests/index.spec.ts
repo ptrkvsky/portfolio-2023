b5ec3153431e98d95a93da6b7b995107ab267162
Kovsky <johan.petrikovsky@gmail.com>
2023-09-17 22:25:56 +0200

    test: :test_tube: add test on logo

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from "@playwright/test";
 
+const homeTitle = "Johan Petrikovsky développeur React Freelance à Toulouse";
+
 test("meta is correct", async ({ page }) => {
   await page.goto("http://localhost:4321/");
 
-  await expect(page).toHaveTitle(
-    "Johan Petrikovsky développeur React Freelance à Toulouse",
-  );
+  await expect(page).toHaveTitle(homeTitle);
 });
 
 test("should find the header element in the DOM", async ({ page }) => {
@@ -24,10 +24,8 @@ test("logo link", async ({ page }) => {
   await page.goto("http://localhost:4321/");
 
   // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
+  await page.getByTestId("header___logo").click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole("heading", { name: "Installation" }),
-  ).toBeVisible();
+  // Expects home page to have a certain title.
+  await expect(page).toHaveTitle(homeTitle);
 });
