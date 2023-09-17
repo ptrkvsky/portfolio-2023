cc33b45056820d31547b57b0fb95446a5f8a1bc3
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2023-09-17 20:00:07 +0200

    feat: add header

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -7,3 +7,15 @@ test("meta is correct", async ({ page }) => {
     "Johan Petrikovsky développeur React Freelance à Toulouse",
   );
 });
+
+test("should find the header element in the DOM", async ({ page }) => {
+  const headerElement = page.getByTestId("header");
+
+  expect(headerElement).not.toBeNull();
+});
+
+test("should find the logo element in the DOM", async ({ page }) => {
+  const logo = page.getByTestId("header___logo");
+
+  expect(logo).not.toBeNull();
+});
